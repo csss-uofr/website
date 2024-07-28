e96a85ec218a2c5e9444ebf809134f37b9c93979
WeSecureYou <devp61136@gmail.com>
2024-07-28 03:36:52 -0600

    Imrpvoed Footer Design, Improved Navbar Design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React from "react";
+import { DiCodepen } from "react-icons/di";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <footer className="text-gray-800 border-t shadow-md bordery-gray-400 p-4 text-center">
-            <div className="max-w-7xl mx-auto">
-                <div className="flex justify-center items-center">
-                    <p className="text-sm font-bold">&copy; {new Date().getFullYear()} Computer Science Students Society, University of Regina. All rights reserved.</p>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center py-12 border-t border-gray-400">
+        <div className="flex-row flex px-6 py-4">
+          <DiCodepen className="text-5xl lg:text-7xl" />
+        </div>
+        <div className="flex-row flex justify-left px-8">
+          <div className="flex-col flex">
+            <h4 className="text-xl font-bold mb-1">Resources</h4>
+            <Link className="text-sm text-gray-700 hover:underline hover:cursor-pointer">Events</Link>
+            <Link className="text-sm text-gray-700 hover:underline hover:cursor-pointer">Resource</Link>
+            <Link className="text-sm text-gray-700 hover:underline hover:cursor-pointer">Partners</Link>
+          </div>
+          <div className="flex-col flex ml-20 sm:ml-24">
+          <h4 className="text-xl font-bold mb-1">About</h4>
+            <Link className="text-sm text-gray-700 hover:underline hover:cursor-pointer">Core Executives</Link>
+            <Link className="text-sm text-gray-700 hover:underline hover:cursor-pointer">Officers</Link>
+            <Link className="text-sm text-gray-700 hover:underline hover:cursor-pointer">Volunteer</Link>
+            <Link className="text-sm text-gray-700 hover:underline hover:cursor-pointer">Meeting Minutes </Link>
+            <Link className="text-sm text-gray-700 hover:underline hover:cursor-pointer">Suggestion Box</Link>
+          </div>
+        </div>
+        <div className="flex flex-row justify-center items-center mt-8 mb-2 md:px-4">
+          <p className="text-sm font-bold text-center">
+            &copy; {new Date().getFullYear()} Computer Science Students Society,
+            University of Regina. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { DiCodepen } from "react-icons/di";
 import { IoSearch } from "react-icons/io5";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
+import { IoMenuSharp } from "react-icons/io5";
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    return (
-        <>
-        <div className="w-full h-20 flex flex-row items-center md:justify-between md:border-none border-gray-300 border-b justify-center px-10">
-<div className=
-"w-max flex flex-row items-center">
-    <div className="text-3xl font-bold tracking-wide flex flex-row items-center hover:cursor-pointer"
-    onClick={() => navigate("/")}>
-        <span><DiCodepen className="text-5xl" /></span>
-        <h1>UR CSSS</h1>
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="w-full h-20 flex flex-row items-center md:justify-between md:border-none border-gray-300 border-b justify-between px-10">
+        <div className="w-max flex flex-row items-center">
+          <div
+            className="text-3xl font-bold tracking-wide flex flex-row items-center hover:cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            <span>
+              <DiCodepen className="text-5xl" />
+            </span>
+            <h1>UR CSSS</h1>
+          </div>
+          <div className="ml-1 text-gray-600 hidden md:flex">
+            <Link className="ml-4" to="/about">
+              About Us
+            </Link>
+            <Link className="ml-4" to="/events">
+              Events
+            </Link>
+            <Link className="ml-4" to="/contact">
+              Contact
+            </Link>
+            <Link className="ml-4" to="/elections">
+              Elections
+            </Link>
+          </div>
         </div>
-    <div className="ml-1 text-gray-600 hidden md:flex">
-        <Link className="ml-4" to="/about">About Us</Link>
-        <Link className="ml-4" to="/events">Events</Link>
-        <Link className="ml-4" to="/contact">Contact</Link>
-        <Link className="ml-4" to="/elections">Elections</Link>
-    </div>
-</div>
-<div className="w-max-c hidden md:flex">
-    <input type="text" placeholder="Search" className="py-1 px-2 text-sm border-black border-b outline-none" />
-    <button className="ml-2"><IoSearch className="text-lg" /></button>
-</div>
+        <div className="md:hidden flex flex-row items-center">
+          <button className="px-2 py-1 border bordery-gray-200 rounded-md">
+            <IoMenuSharp className="text-2xl" />
+          </button>
         </div>
-        </>
-    );
+        <div className="w-max-c hidden md:flex">
+          <input
+            type="text"
+            placeholder="Search"
+            className="py-1 px-2 text-sm border-black border-b outline-none"
+          />
+          <button className="ml-2">
+            <IoSearch className="text-lg" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
